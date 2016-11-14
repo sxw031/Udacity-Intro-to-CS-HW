c345f3cd1e9449f346dd12b0c0542456681d9e2a
James Wang <sxw031@Jamess-MacBook-Pro-2.local>
2016-11-14 23:09:00 +0800

    first update for the problem sets and solutions

---
+++ lesson2/rangeofset.py
@@ -0,0 +1,34 @@
+# The range of a set of values is the maximum value minus the minimum
+# value. Define a procedure, set_range, which returns the range of three input
+# values.
+
+# Hint: the procedure, biggest which you coded in this unit
+# might help you with this question. You might also like to find a way to
+# code it using some built-in functions.
+def bigger(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+        
+def smaller(a,b):
+    if a > b:
+        return b
+    else:
+        return a
+    
+def biggest(a,b,c):
+    return bigger(bigger(a,b),c)
+
+def smallest(a,b,c):
+    return smaller(smaller(a,b),c)
+        
+def set_range(a,b,c):
+    return biggest(a,b,c) - smallest(a,b,c)
+    
+
+print set_range(10, 4, 7)
+#>>> 6  # since 10 - 4 = 6
+
+print set_range(1.1, 7.4, 18.7)
+#>>> 17.6 # since 18.7 - 1.1 = 17.6
